bed93abe316a0e2604f80a34b432978aed21a7f4
alisondraV <alice.7414122013@gmail.com>
2021-12-04 12:21:40 -0500

    Remove HomeActivity and add options menu with logout option

--- app/src/main/java/com/example/memorieskeeper/ListActivity.java
+++ app/src/main/java/com/example/memorieskeeper/ListActivity.java
@@ -3,11 +3,14 @@ package com.example.memorieskeeper;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
+import com.example.memorieskeeper.services.CustomGoogleAuth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -15,9 +18,10 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.memorieskeeper.databinding.ActivityListBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ListActivity extends AppCompatActivity {
-
     private AppBarConfiguration appBarConfiguration;
     private ActivityListBinding binding;
 
@@ -42,5 +46,31 @@ public class ListActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_list);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            CustomGoogleAuth.getGoogleSignInClient(this).signOut().addOnCompleteListener(result -> {
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            });
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     }
 }
