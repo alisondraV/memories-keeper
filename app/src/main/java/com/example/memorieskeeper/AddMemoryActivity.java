ce28b27d39b884ab7125fc97de7245b71374e5af
alisondraV <alice.7414122013@gmail.com>
2021-12-04 18:16:58 -0500

    Make AddMemoryActivity functional

--- app/src/main/java/com/example/memorieskeeper/AddMemoryActivity.java
+++ app/src/main/java/com/example/memorieskeeper/AddMemoryActivity.java
@@ -1,14 +1,81 @@
 package com.example.memorieskeeper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AddMemoryActivity extends AppCompatActivity {
+    TextView txtName, txtDescription, txtLocation;
+    Button btnAddMemory;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    FirebaseUser user;
+
+    long maxId = 0;
+
+    ValueEventListener onButtonClickEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+            Toast.makeText(AddMemoryActivity.this, "Memory has been successfully created!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+            Toast.makeText(AddMemoryActivity.this, "An error occurred while creating a memory :(", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_memory);
+
+        txtName = findViewById(R.id.txtName);
+        txtDescription = findViewById(R.id.txtDescription);
+        txtLocation = findViewById(R.id.txtLocation);
+        btnAddMemory = findViewById(R.id.btnAddMemory);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Memories");
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                maxId = snapshot.getChildrenCount();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) { }
+        });
+
+        btnAddMemory.setOnClickListener(view -> {
+            MemoryModel newMemory = new MemoryModel(
+                    user.getUid(),
+                    String.valueOf(txtName.getText()),
+                    String.valueOf(txtDescription.getText()),
+                    String.valueOf(txtLocation.getText()));
+            databaseReference.child(String.valueOf(maxId + 1)).setValue(newMemory);
+
+            databaseReference.addValueEventListener(onButtonClickEventListener);
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        databaseReference.removeEventListener(onButtonClickEventListener);
     }
 }
