90bb0791f0218d23597834c8b4c317f66bbc69e4
alisondraV <alice.7414122013@gmail.com>
2021-12-07 16:57:54 -0500

    Add needed fields to the Memory page

--- app/src/main/java/com/example/memorieskeeper/MemoryFragment.java
+++ app/src/main/java/com/example/memorieskeeper/MemoryFragment.java
@@ -1,9 +1,14 @@
 package com.example.memorieskeeper;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,8 +18,13 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.memorieskeeper.databinding.FragmentMemoryBinding;
 
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MemoryFragment extends Fragment {
-    TextView txtMemoryName;
+    TextView txtMemoryName, txtMemoryCreated, txtMemoryLocation, txtMemoryDescription;
+    ImageView imgMemory;
     private FragmentMemoryBinding binding;
 
     @Override
@@ -30,6 +40,11 @@ public class MemoryFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         txtMemoryName = view.findViewById(R.id.txtMemoryName);
+        txtMemoryCreated = view.findViewById(R.id.txtMemoryCreated);
+        txtMemoryLocation = view.findViewById(R.id.txtMemoryLocation);
+        txtMemoryDescription = view.findViewById(R.id.txtMemoryDescription);
+        imgMemory = view.findViewById(R.id.imgMemory);
+
         if (getArguments() == null) {
             Toast.makeText(MemoryFragment.super.getContext(), "Couldn't get the memory :(", Toast.LENGTH_SHORT).show();
             NavHostFragment
@@ -39,17 +54,43 @@ public class MemoryFragment extends Fragment {
         }
 
         MemoryModel memory = getArguments().getParcelable("memory");
-        txtMemoryName.setText(memory.getName());
+        String dateString = new SimpleDateFormat("MM/dd/yyyy").format(new Date(memory.getCreatedAt()));
 
-        binding.buttonPrevious.setOnClickListener(view1 -> NavHostFragment
-                .findNavController(MemoryFragment.this)
-                .popBackStack()
-        );
+        txtMemoryName.setText(memory.getName());
+        txtMemoryCreated.setText(dateString);
+        txtMemoryLocation.setText(memory.getLocation());
+        txtMemoryDescription.setText(memory.getDescription());
+        new DownloadImageTask(imgMemory).execute(memory.getImageUrl());
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap bmp = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                bmp = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return bmp;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
